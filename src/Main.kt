90b37830d6558501aaef37333d16a90104439773
jjy <skwntnftkgk@gmail.com>
2020-10-20 18:35:58 +0900

    크레인

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
-import failure_rate.Solution
+import base_3_flip.Solution
 
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             val solution = Solution()
-            println(solution.solution(5, intArrayOf(2,1,2,6,2,4,3,3)).joinToString { "$it" })
-            println(solution.solution(4, intArrayOf(3,3,3,3,3)).joinToString { "$it" })
+            println(solution.solution(45))
+            println(solution.solution(125))
         }
     }
 }
--- src/crane_doll_draw_game/Solution.kt
+++ src/crane_doll_draw_game/Solution.kt
@@ -1,16 +1,18 @@
 package crane_doll_draw_game
 
+import java.util.*
+
 class Solution {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
         var answer = 0
-        val basket = mutableListOf<Int>()
+        val basket = Stack<Int>()
         val temp = mutableListOf<MutableList<Int>>()
         board.reverse()
         board.forEachIndexed { index, ints ->
             for (i in board.indices) {
                 if (temp.size == i) temp.add(mutableListOf())
                 val current = board[i][index]
-                if(current == 0) continue
+                if (current == 0) continue
                 temp[index].add(current)
             }
         }
@@ -21,14 +23,13 @@ class Solution {
             println("index : $it = $temp - $current / basket : $basket")
             temp[it - 1].removeAt(currentLine.size - 1)
             if (basket.isNotEmpty()) {
-                val last = basket.last()
-                if (last == current) {
-                    basket.remove(last)
+                if (basket.peek() == current) {
+                    basket.pop()
                     answer += 2
                     return@forEach
                 }
             }
-            basket.add(current)
+            basket.push(current)
         }
         return answer
     }
